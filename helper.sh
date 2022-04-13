0b46c1222c5d356d4354a3402bf2d9ed55d4aef5
Joshua Gourneau <josh@gourneau.com>
2022-04-12 22:58:36 -0700

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -ex
-t-rex --mclock 590 --fan 100 -a ethash -o stratum+tcp://eth-us-west.flexpool.io:4444 -u ${1}.${VAST_CONTAINERLABEL}_${2}MH
+t-rex --mclock 590 -a ethash -o stratum+tcp://eth-us-west.flexpool.io:4444 -u ${1}.${VAST_CONTAINERLABEL}_${2}MH
